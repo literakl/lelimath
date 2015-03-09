00f9f1b71afafcbbc7407514cd5b7b066cfbf3e1
Leos Literak <leos@literak.cz>
2015-03-09 05:49:51 +0000

    Pokud uživatel nakliká moc písmen, nebude se neznámé políčko roztahovat na úkor dalších políček v žádném směru. Stylování neznámého políčka.
    git-svn-id: https://www.literak.cz/svn/lelisoft/LeliMath@20 da24d14a-7dfa-43c6-8752-2aa101e44d5d

--- app/src/main/java/lelisoft/com/lelimath/activities/CalcActivity.java
+++ app/src/main/java/lelisoft/com/lelimath/activities/CalcActivity.java
@@ -1,8 +1,6 @@
 package lelisoft.com.lelimath.activities;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,14 +39,18 @@ public class CalcActivity extends Activity {
         unknown.setText(formula.getUserEntry());
     }
 
-    public void operandClicked(View view) {
+    public void operatorClicked(View view) {
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
-        prepareNewFormula();
+        if (formula == null) {
+            prepareNewFormula();
+        }
+//        SharedPreferences mPrefs = getSharedPreferences();
+//        mCurViewMode = mPrefs.getInt("view_mode", DAY_VIEW_MODE);
     }
 
     private void prepareNewFormula() {
@@ -82,7 +84,7 @@ public class CalcActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_calc, menu);
-        ActionBar actionBar = getActionBar();
+//        ActionBar actionBar = getActionBar();
 //        actionBar.setDisplayShowHomeEnabled(true);setHomeButtonEnabled(true)
 //        actionBar.setIcon(R.drawable.ic_launcher);
         return true;
