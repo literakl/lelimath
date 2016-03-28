4aa41637c7b21f710bf5c032780657fb7e8f7ea0
Leos Literak <leos@literak.cz>
2016-03-28 09:09:15 +0000

    pokus
    git-svn-id: https://www.literak.cz/svn/lelisoft/LeliMath@92 da24d14a-7dfa-43c6-8752-2aa101e44d5d

--- app/src/androidTest/java/lelisoft/com/lelimath/ApplicationTest.java
+++ app/src/androidTest/java/lelisoft/com/lelimath/ApplicationTest.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
+ * Desc
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
